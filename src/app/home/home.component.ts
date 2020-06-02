0c3cfcdadc98cf734b649e771ae1f0f5b8d01480
learningStar <sheetalbhati546@gmail.com>
2020-06-02 13:45:21 +0530

    promise(when result is immediately available)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,9 +20,12 @@ leader:Leader;
   private promotionService:PromotionService,private leaderService:LeaderService) { }
 
   ngOnInit() {
-  this.dish=this.dishService.getFeaturedDish();
-  this.promotion=this.promotionService.getFeaturedPromotion();
-  this.leader=this.leaderService.getFeaturedLeader();
+  this.dishService.getFeaturedDish()
+  .then((dish)=> this.dish=dish);
+  this.promotionService.getFeaturedPromotion()
+  .then((promotion)=> this.promotion=promotion);
+  this.leaderService.getFeaturedLeader()
+  .then((leader)=> this.leader=leader);
   
   }
 
